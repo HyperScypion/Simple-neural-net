2dad803b602911693f487cfaab46048ee0cd8eef
Tomasz Derek <t.derek@g.loiv.torun.pl>
2018-10-07 14:18:17 +0200

    Update net.py

--- src/net.py
+++ src/net.py
@@ -2,7 +2,24 @@ import random
 import pandas
 import numpy as np 
 
-class MLP(object):
+dataset = np.array(([1, 0], [0, 0], [1, 1]), dtype=float)
 
-def sigma(x):
-    return (1 / (1 + np.exp(-x)))
+output = np.array(([0, 0, 1], dtype=float) 
+
+class MLP(object):
+    def __init__(self, inputLayer, hiddenLayer, outputLayer):
+        
+        self.inputLayer = inputLayer
+        self.hiddenLayer = hiddenLayer
+        self.outputLayer = outputLayer
+
+        self.weights0 = np.random.rand(self.inputLayer, self.hiddenLayer)
+        self.weights1 = np.random.rand(self.hiddenLayer, self.outputLayer)
+    
+    def forward()
+
+    def sigma(x):
+        return (1 / (1 + np.exp(-x))
+    
+    def sigmaPrime(x):
+        return x * (1 - x)
