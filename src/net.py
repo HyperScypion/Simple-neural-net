5c729694733b09a67c36385bfb318538815cdab3
hyperscypion <tomasz_derek@wp.pl>
2018-10-15 22:03:14 +0200

    forward

--- src/net.py
+++ src/net.py
@@ -16,7 +16,14 @@ class MLP(object):
         self.weights0 = np.random.rand(self.inputLayer, self.hiddenLayer)
         self.weights1 = np.random.rand(self.hiddenLayer, self.outputLayer)
     
-    def forward()
+    def forward(self):
+    
+        self.z = sigma(np.dot(inputLayer, weights0))
+        self.z2 = sigma(np.dot(z2, weights1))
+        return self.z2
+
+    def backprop(self):
+     
 
     def sigma(x):
         return (1 / (1 + np.exp(-x))
