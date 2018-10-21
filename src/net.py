765212a599ef2b678c555d611ae89c8c864a3f83
hyperscypion <tomasz_derek@wp.pl>
2018-10-22 01:46:55 +0200

    final

--- src/net.py
+++ src/net.py
@@ -1,54 +1,54 @@
 import random
-import pandas
-import numpy as np 
+import numpy as np
 
-dataset = np.array(([1, 0], [0, 0], [1, 1]), dtype=float)
+# X = (hours sleeping, hours studying), y = score on test
+X = np.array(([0, 0], [1, 0], [0, 1]), dtype=float)
+y = np.array(([1], [0], [0]), dtype=float)
 
-output = np.array(([0, 0, 1], dtype=float) 
-def sigma(x):
-	return (1 / 1 + np.exp(-x))
-		
-def SigmaPrime(x):
-	return x + (1 - x)
-		
 class MultiLayerPerceptron():
-	
-	def __init__(self, inputLayer, hiddenLayer, outputLayer):
-		self.inputLayer = inputLayer
-		self.hiddenLayer = hiddenLayer
-		self.outputLayer = outputLayer
-	
-		self.weight0 = np.random.rand(self.inputLayer, self.hiddenLayer) 
-		self.weight1 = np.random.rand(self.hiddenLayer, self.outputLayer)
-	
-		print("Pierwsza macierz wag")
-		print(self.weight0)
-		print("Druga macierz wag")
-		print(self.weight1)
-	
-	def forward(self, input):
-		
-		self.hidden = sigma(np.dot(input, self.weight0))
-		print("First layer")
-		print(self.hidden)
-		self.out = sigma(np.dot(self.hidden, self.weight1))
-		print("Output")
-		print(self.out)
-		
-		
-	def backprop(self, input, output):
-		print("backprop")
-		# weight1B = np.dot(self.hidden, (2*(output - self.out) * SigmaPrime(self.out)))
-		# weight0B = np.dot(input.T, (np.dot(2*(output - self.out) * SigmaPrime(self.out), self.weight1.T) * SigmaPrime(self.hidden)))
-		# self.weight0 += weight0B
-		# self.weight1 += weight1B
-	
+  def __init__(self, inputSize, hiddenSize, outputSize):
+    
+    self.inputSize = inputSize
+    self.outputSize = outputSize
+    self.hiddenSize = hiddenSize
 
-	def train(self, input, output):
-		print("train")
-		
-net = MultiLayerPerceptron(3, 4, 2)
-dataset = np.array([1, 2, 3])
-net.forward(dataset)
-output = np.array([1, 2])
-# net.backprop(dataset, output)
+    
+    self.weight0 = np.random.rand(self.inputSize, self.hiddenSize) 
+    print("Weight0: " + str(self.weight0))
+    self.weight1 = np.random.rand(self.hiddenSize, self.outputSize)
+    print("Weight1: " + str(self.weight1))
+
+  def forward(self, X):
+    
+    self.hidden = self.sigma(np.dot(X, self.weight0))  
+    o = self.sigma(np.dot(self.hidden, self.weight1)) 
+    return o 
+
+  def sigma(self, x):
+     
+    return 1/(1+np.exp(-x))
+
+  def sigmaPrime(self, x):
+    
+    return x * (1 - x)
+
+  def backward(self, inputX, output, o):
+   
+    self.o_error = output - o
+    self.o_delta = self.o_error*self.sigmaPrime(o) 
+    self.z2_error = self.o_delta.dot(self.weight1.T) 
+    self.z2_delta = self.z2_error*self.sigmaPrime(self.hidden)
+
+    self.weight0 += inputX.T.dot(self.z2_delta) 
+    self.weight1 += self.hidden.T.dot(self.o_delta) 
+
+  def train (self, inputX, output):
+    o = self.forward(inputX)
+    self.backward(inputX, output, o)
+
+nn = MultiLayerPerceptron(2, 2, 1)
+for i in range(2000): 
+  nn.train(X, y)
+
+print(nn.forward([0, 0]))
+print(nn.forward([1, 1]))
